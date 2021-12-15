c265d6a7dd3f377446e1370580f9dace13e2d72b
dekkuri <59847357+dekkuri@users.noreply.github.com>
2021-12-14 20:43:31 -0500

    Done step 2.

--- js/script.js
+++ js/script.js
@@ -99,12 +99,15 @@ function buildAndShowHomeHTML (categories) {
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (homeHtml) {
+    function (homeHtmlUrl) {
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+      var chosenCategoryShortName = 
+        chooseRandomCategory(categories)
+      },
+      false);
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
