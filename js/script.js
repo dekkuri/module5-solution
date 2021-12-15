dd3537896184a0384868e52a52c2d254d05d6c92
dekkuri <59847357+dekkuri@users.noreply.github.com>
2021-12-15 10:33:01 -0500

    Update script.js
    Deleted all 'homeHtmlUrl' and fix to 'homeHtml'

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 var dc = {};
 
-var homeHtmlUrl = "snippets/home-snippet.html";
+var homeHtml = "snippets/home-snippet.html";
 var allCategoriesUrl =
   "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
@@ -98,7 +98,7 @@ function buildAndShowHomeHTML (categories) {
 
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
-    homeHtmlUrl,
+    homeHtml,
     function (homeHtml) {
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
