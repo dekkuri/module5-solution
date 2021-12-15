3c2d8f2d352ea7e1c6dde1308effff178fcc017c
dekkuri <59847357+dekkuri@users.noreply.github.com>
2021-12-14 21:36:37 -0500

    edit homeHtml to homeHtmlUrl.

--- js/script.js
+++ js/script.js
@@ -99,7 +99,7 @@ function buildAndShowHomeHTML (categories) {
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (homeHtml) {
+    function (homeHtmlUrl) {
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
@@ -120,7 +120,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage = 
-        insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+        insertProperty(homeHtmlUrl, "randomCategoryShortName", chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
